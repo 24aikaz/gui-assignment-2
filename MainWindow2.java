d19407bb307830549f37793765e1069b33deb1d0
Keea <zak.owasil@gmail.com>
2023-08-24 09:11:08 +0400

    Added functionallity for Shapes, Color and Clip.

--- MainWindow2.java
+++ MainWindow2.java
@@ -19,6 +19,10 @@ public class MainWindow2 extends JFrame {
 
     drawShape ds;
 
+    drawClippingWindow cw;
+
+    Color selectedColor;
+
     JFrame frame; // the main frame
 
     // Title bar buttons
@@ -186,7 +190,6 @@ public class MainWindow2 extends JFrame {
     public class menuCollapse implements ActionListener{
         public void actionPerformed(ActionEvent e) {
 			full_menu_panel.setVisible(false);
-            transformation_panel.setVisible(false);
             CollapsedMenu();
 		}
     }
@@ -203,25 +206,32 @@ public class MainWindow2 extends JFrame {
 		}
 	}
 
-
     public void MenuDetails(){
         JPanel tools_container = new JPanel(new GridLayout(1, 5, 10, 10));
-        
+        tools_container.setBackground(bluish_gray);
+
         // SHAPES
         JPanel shapes_panel = new JPanel(new BorderLayout());
-        JLabel shapes_label = new JLabel("Shape");
+        shapes_panel.setBackground(bluish_gray);
+        JLabel shapes_label = new JLabel("Shapes", SwingConstants.CENTER);
         shapes_panel.add(shapes_label, BorderLayout.NORTH);
         JPanel shapes_container = new JPanel(new GridLayout(2, 2));
+        shapes_container.setBackground(bluish_gray);
+
         line_btn = new JButton(new ImageIcon(new ImageIcon("icons/slash-solid.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));
+        line_btn.addActionListener(new drawingToolHandler());
         shapes_container.add(line_btn);
 
         circle_btn = new JButton(new ImageIcon(new ImageIcon("icons/circle-regular.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));
+        circle_btn.addActionListener(new drawingToolHandler());
         shapes_container.add(circle_btn);
 
         square_btn = new JButton(new ImageIcon(new ImageIcon("icons/square-regular.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));
+        square_btn.addActionListener(new drawingToolHandler());
         shapes_container.add(square_btn);
 
         polygon_btn = new JButton(new ImageIcon(new ImageIcon("icons/draw-polygon-solid.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));
+        polygon_btn.addActionListener(new drawingToolHandler());
         shapes_container.add(polygon_btn);
 
         shapes_panel.add(shapes_container, BorderLayout.CENTER);
@@ -229,25 +239,30 @@ public class MainWindow2 extends JFrame {
 
         // COLOUR
         JPanel colour_panel = new JPanel(new BorderLayout());
-        JLabel colour_label = new JLabel("Colour");
+        colour_panel.setBackground(bluish_gray);
+        JLabel colour_label = new JLabel("Colour", SwingConstants.CENTER);
         colour_panel.add(colour_label, BorderLayout.NORTH);
         JPanel colour_container = new JPanel(new GridLayout(2, 1));
+        colour_container.setBackground(bluish_gray);
 
         color_picker_btn = new JButton("Choose color");
+        color_picker_btn.addActionListener(new btnColorPickerHandler());
         colour_container.add(color_picker_btn);
 
         apply_color_btn = new JButton(new ImageIcon(new ImageIcon("icons/paint-roller-solid.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));
+        apply_color_btn.addActionListener(new btnFillHandler());
         colour_container.add(apply_color_btn);
 
         colour_panel.add(colour_container, BorderLayout.CENTER);
-
         tools_container.add(colour_panel);
 
         // TRANSFORMATION
         JPanel transform_panel = new JPanel(new BorderLayout());
-        JLabel transform_label = new JLabel("Transform");
+        transform_panel.setBackground(bluish_gray);
+        JLabel transform_label = new JLabel("Transform", SwingConstants.CENTER);
         transform_panel.add(transform_label, BorderLayout.NORTH);
         JPanel transform_container = new JPanel(new GridLayout(2, 1));
+        transform_container.setBackground(bluish_gray);
 
         transform_picker_btn = new JButton("Choose transformation");
         transform_picker_btn.addActionListener(new transformDetails());
@@ -260,15 +275,20 @@ public class MainWindow2 extends JFrame {
         tools_container.add(transform_panel);
 
         // CLIPPING
+        cw = new drawClippingWindow(null, null, null);
         JPanel clip_panel = new JPanel(new BorderLayout());
-        JLabel clip_label = new JLabel("Clip");
+        clip_panel.setBackground(bluish_gray);
+        JLabel clip_label = new JLabel("Clip", SwingConstants.CENTER);
         clip_panel.add(clip_label, BorderLayout.NORTH);
         JPanel clip_container = new JPanel(new GridLayout(2, 1));
+        clip_container.setBackground(bluish_gray);
 
         clip_btn = new JButton("Start clipping");
+        clip_btn.addActionListener(new btnClippingHandler());
         clip_container.add(clip_btn);
 
         apply_clip_btn = new JButton(new ImageIcon(new ImageIcon("icons/crop-simple-solid.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));
+        apply_clip_btn.addActionListener(new btnClippingHandler());
         clip_container.add(apply_clip_btn);
 
         clip_panel.add(clip_container);
@@ -277,9 +297,11 @@ public class MainWindow2 extends JFrame {
 
         // ANIMATION
         JPanel animate_panel = new JPanel(new BorderLayout());
-        JLabel animate_label = new JLabel("Animation");
+        animate_panel.setBackground(bluish_gray);
+        JLabel animate_label = new JLabel("Animation", SwingConstants.CENTER);
         animate_panel.add(animate_label, BorderLayout.NORTH);
         JPanel animate_container = new JPanel(new GridLayout(1, 2));
+        animate_container.setBackground(bluish_gray);
 
         play_btn = new JButton(new ImageIcon(new ImageIcon("icons/play-solid.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));
         animate_container.add(play_btn);
@@ -325,6 +347,66 @@ public class MainWindow2 extends JFrame {
             transformation_panel.setVisible(false);
 		}
     } 
+
+    public class btnColorPickerHandler implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JColorChooser colorChooser = new JColorChooser();
+			MainWindow2.this.selectedColor = JColorChooser.showDialog(null, "Choose fill color", selectedColor);
+		}
+	}
+
+    public class btnFillHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            drawing_panel.removeAll();
+            drawing_panel.add(new ScanLine(ds.getXPoints(), ds.getYPoints(), selectedColor));
+            drawing_panel.repaint();
+            drawing_panel.revalidate();
+        }
+    }
+
+    public class btnClippingHandler implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == clip_btn) {
+				drawing_panel.removeAll();
+				cw = new drawClippingWindow(ds.getXPoints(), ds.getYPoints(), ds.getShape());
+				drawing_panel.add(cw);
+			}
+			if (e.getSource() == apply_clip_btn) {
+				if (cw.getxMin()==-1 || cw.getyMin()==-1 || cw.getxMax()==-1 || cw.getyMax()==-1) {
+					JOptionPane.showMessageDialog(MainWindow2.this, "Clipping Window should be drawn first.");
+				}
+				else {
+					drawing_panel.removeAll();
+					drawing_panel.add(new LineClippingPanel(cw.getxMin(), cw.getyMin(), cw.getxMax(), cw.getyMax(), ds.getXLines(),ds.getYLines()));
+					cw.initXY();
+				}
+			}
+			drawing_panel.revalidate();
+			drawing_panel.repaint();
+		}
+	}
+
+    public class drawingToolHandler implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			drawing_panel.removeAll();
+			if (e.getSource()==square_btn) {
+				ds = new drawShape("rectangle");
+			}
+			else if (e.getSource()==circle_btn) {
+				ds = new drawShape("circle");
+			}
+			else if (e.getSource()==line_btn) {
+				ds = new drawShape("line");
+			}
+			else if (e.getSource()==polygon_btn) {
+				JOptionPane.showMessageDialog(MainWindow2.this, "Left-click to place vertices.\nWhen you are done, Right-click to stop and complete the polygon.");
+				ds = new drawShape("polygon");
+			}
+			drawing_panel.add(ds);
+			repaint();
+			revalidate();
+		}
+	}
     
 
     public static void main(String[] args) {
